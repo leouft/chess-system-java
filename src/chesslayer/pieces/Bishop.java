2f1f6f1af0713d902dc01c1a46718142c7f1892d
leouft <leonardo.soares1@mail.uft.edu.br>
2026-06-02 09:35:52 -0300

    Remoção de peças não implementadas ainda

--- src/chesslayer/pieces/Bishop.java
+++
@@ -1,6 +0,0 @@
-package chesslayer.pieces;
-
-import chesslayer.ChessPiece;
-
-public class Bishop extends ChessPiece {
-}
--- src/chesslayer/pieces/Knight.java
+++
@@ -1,6 +0,0 @@
-package chesslayer.pieces;
-
-import chesslayer.ChessPiece;
-
-public class Knight extends ChessPiece {
-}
