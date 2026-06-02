42195007f57aba780c53a7d141b480dec2bd09dd
leouft <leonardo.soares1@mail.uft.edu.br>
2026-06-01 23:17:13 -0300

    Implementação das funções de Piece

--- src/boardlayer/Piece.java
+++ src/boardlayer/Piece.java
@@ -3,4 +3,26 @@ package boardlayer;
 public abstract class Piece {
     protected Position position;
     private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] aux = possibleMoves();
+        for (int i = 0; i < aux.length; i++) {
+            for (int j = 0; j < aux.length; j++) {
+                if (aux[i][j])
+                    return true;
+            }
+        }
+        return false;
+    }
 }
