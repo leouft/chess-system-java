0bfbdea562fb88e668cf6bb6fd28458b9e46ac3f
leouft <leonardo.soares1@mail.uft.edu.br>
2026-06-02 11:22:40 -0300

    Colors added

--- src/application/UI.java
+++ src/application/UI.java
@@ -4,12 +4,22 @@ import chesslayer.ChessPiece;
 import chesslayer.Color;
 
 public class UI {
+    public static final String RESET = "\u001B[0m";
+    public static final String WHITE = "\u001B[37m";
+    public static final String BLACK = "\u001B[30m";
+
+    public static final String BLUE_BACKGROUND = "\u001B[44m";
+
+
     public static void printPiece(ChessPiece piece) {
         if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + RESET);
         }
         else {
-            System.out.print(piece);
+            if (piece.getColor() == Color.WHITE)
+                System.out.print(WHITE + piece + RESET);
+            else
+                System.out.print(BLACK + piece + RESET);
         }
         System.out.print(" ");
     }
