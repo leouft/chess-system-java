e817f9042afb32efbccd2c028fb7985b49f4a08c
VexVinny <viniciusdesousa220@gmail.com>
2026-06-02 01:00:46 -0300

    implementação da pieces/Rock

--- src/chesslayer/pieces/Rock.java
+++ src/chesslayer/pieces/Rock.java
@@ -3,4 +3,50 @@ package chesslayer.pieces;
 import chesslayer.ChessPiece;
 
 public class Rock extends ChessPiece {
+
+    public Rock(Board board, Color color){
+        super(board, color);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] aux = new boolean[getBoard().getRows()][getBoard().getColuns()];
+
+        Position pos = new Position(0, 0);
+
+        //movimentos da torre
+        //cima
+        pos.setValue(position.getRow() - 1, position.getColuns()){
+            while(getBoard().positionExist(pos) && !getBoard().thereIsAPiece(pos)){
+                aux[pos.getRow()][pos.getColumn()] = true;
+                pos.setRow(pos.getRow() - 1);
+            }
+        }
+
+        //baixo
+        pos.setValue(position.getRow() + 1, position.getColuns()){
+            while(getBoard().positionExist(pos) && !getBoard().thereIsAPiece(pos)){
+                aux[pos.getRow()][pos.getColumn()] = true;
+                pos.setRow(pos.getRow() + 1);
+            }
+        }
+
+        //direita
+        pos.setValue(position.getRow(), position.getColuns() + 1){
+            while(getBoard().positionExist(pos) && !getBoard().thereIsAPiece(pos)){
+                aux[pos.getRow()][pos.getColumn()] = true;
+                pos.setRow(pos.getColuns() + 1);
+            }
+        }
+
+        //esquerda
+        pos.setValue(position.getRow(), position.getColuns() - 1){
+            while(getBoard().positionExist(pos) && !getBoard().thereIsAPiece(pos)){
+                aux[pos.getRow()][pos.getColumn()] = true;
+                pos.setRow(pos.getColuns() - 1);
+            }
+        }
+
+    }
+
 }
