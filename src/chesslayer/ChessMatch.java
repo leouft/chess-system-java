bac4c2fffcb750078da5c6dcc65b400ed6cbb771
VexVinny <viniciusdesousa220@gmail.com>
2026-06-01 23:47:01 -0300

    implementação de metodos de ChessMatch

--- src/chesslayer/ChessMatch.java
+++ src/chesslayer/ChessMatch.java
@@ -1,4 +1,29 @@
 package chesslayer;
 
 public class ChessMatch {
+
+    private int turn;
+    private Color currentPlayer;
+    private boolean check;
+    private boolean checkMate;
+    private ChessPiece enPassantVulnerable;
+    private ChessPiece promoted;
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+
+    }
+
+    public ChessPiece performChessMove(
+            ChessPosition sourcePosition,
+            ChessPosition targetPosition) {
+
+    }
+
+    public ChessPiece replacePromotedPiece(String type) {
+
+    }
 }
