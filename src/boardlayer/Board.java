7c13f88cc6c282713c6d1b00b9b975113169041c
leouft <leonardo.soares1@mail.uft.edu.br>
2026-06-02 10:14:01 -0300

    Correção de lógica e adição de BoardExceptions

--- src/boardlayer/Board.java
+++ src/boardlayer/Board.java
@@ -6,9 +6,10 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1)
+            throw new BoardException("Board need be at least 1 row and 1 column.");
         this.rows = rows;
         this.columns = columns;
-
         pieces = new Piece[rows][columns];
     }
 
@@ -53,7 +54,7 @@ public class Board {
     }
 
     public boolean positionExists(Position position) {
-        return (position.getRow() > 0 && position.getRow() <= rows && position.getColumn() > 0 && position.getColumn() <= columns);
+        return (position.getRow() >= 0 && position.getRow() < rows && position.getColumn() >= 0 && position.getColumn() < columns);
     }
 
     public boolean thereIsAPiece(Position position) {
