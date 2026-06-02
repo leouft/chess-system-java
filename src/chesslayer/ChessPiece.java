c7cc0a9bcb6603691bca20a841c68143fe07cd02
leouft <leonardo.soares1@mail.uft.edu.br>
2026-06-01 23:34:43 -0300

    Implementação dos métodos de ChessPiece

--- src/chesslayer/ChessPiece.java
+++ src/chesslayer/ChessPiece.java
@@ -1,8 +1,32 @@
 package chesslayer;
 
-public class ChessPiece {
+import boardlayer.Board;
+import boardlayer.Piece;
+import boardlayer.Position;
+
+public abstract class ChessPiece extends Piece {
     private Color color;
     private int moveCount;
 
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
 
+    public Color getColor() {
+        return color;
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece piece = (ChessPiece)getBoard().piece(position);
+        return piece != null && piece.getColor() != color;
+    }
+
+    protected void increaseMoveCount() {
+        moveCount++;
+    }
+
+    protected void decreaseMoveCount() {
+        moveCount--;
+    }
 }
