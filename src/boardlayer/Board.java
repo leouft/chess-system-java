f2dd8d74c02fe4a8cd0c3dee6302211dfd0d7684
leouft <leonardo.soares1@mail.uft.edu.br>
2026-06-01 22:58:06 -0300

    Implementação dos métodos de ce Board

--- src/boardlayer/Board.java
+++ src/boardlayer/Board.java
@@ -5,11 +5,52 @@ public class Board {
     private int columns;
     private Piece[][] pieces;
 
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+
+        pieces = new Piece[rows][columns];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return  columns;
+    }
+
     public Piece piece(int row, int columns) {
         return pieces[row][columns];
     }
 
     public Piece piece(Position position) {
         return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public void placePiece(Piece piece, Position position) {
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+
+    public Piece removePiece(Position position) {
+        if (pieces[position.getRow()][position.getRow()] == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getRow()] = null;
+        return aux;
+    }
+
+    public boolean positionExists(Position position) {
+        return (position.getRow() > 0 && position.getRow() <= rows && position.getColumn() > 0 && position.getColumn() <= columns);
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            return false;
+        }
+        return piece(position) != null;
     }
 }
