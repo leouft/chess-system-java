f1127d2df9c1f198f366fcc10af46b65b23ca1a1
leouft <leonardo.soares1@mail.uft.edu.br>
2026-06-02 10:29:35 -0300

    ChessPosition para conversões

---
+++ src/chesslayer/ChessPosition.java
@@ -0,0 +1,31 @@
+package chesslayer;
+
+import boardlayer.Position;
+
+public class ChessPosition {
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8)
+            throw new ChessException("Invalid ChessPosition. Valid range: a1 to h8.");
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn(){
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition(){ // Converte posições a1, a2, b1... para 0, 0; 0,1; 1;0...
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position) { // Converte posições 0,0; 0,1; 1;0... para a1, a2, b1...
+        return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
+    }
+}
